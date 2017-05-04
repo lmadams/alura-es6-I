cb990ce1fbcad7c73ae5b7ef352a21b1076a4f52
Lucas Adams <adams.lucass@gmail.com>
2017-05-04 00:12:44 -0300

    view e reduce

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -5,21 +5,39 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+
+        this._listaNegociacoes = new ListaNegociacoes();
+        this._negocoesView = new NegociacoesView($('#negociacoesView'));
+
+        this._negocoesView.update(this._listaNegociacoes);
     }
 
     adicona(event) {
         event.preventDefault();
 
-        let negociacao = new Negociacao(
+        let negociacao = this._criaNegociacao();
+
+        this._listaNegociacoes.adiciona(negociacao);
+        this._negocoesView.update(this._listaNegociacoes);
+        this._limpaFormulario();
+    }
+
+    _criaNegociacao() {
+        return new Negociacao(
             DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value
         );
+    }
 
-        console.log(negociacao);
+    _limpaFormulario() {
 
-        let diaMesAno = DateHelper.dataParaTexto(negociacao.data);
+        /** Limpar os campos */
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0;
 
-        console.log(diaMesAno);
+        /** Foco no elemento */
+        this._inputData.focus();
     }
 }
