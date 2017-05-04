f76962823d57c0fbfddad4bac84d73223c61e5e6
Lucas Adams <adams.lucass@gmail.com>
2017-05-04 18:08:29 -0300

    final modulo

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -7,9 +7,13 @@ class NegociacaoController {
         this._inputValor = $('#valor');
 
         this._listaNegociacoes = new ListaNegociacoes();
+        this._mensagem = new Mensagem();
+
         this._negocoesView = new NegociacoesView($('#negociacoesView'));
+        this._mensagemView = new MensagemView($('#mensagemView'));
 
         this._negocoesView.update(this._listaNegociacoes);
+        this._mensagemView.update(this._mensagem);
     }
 
     adicona(event) {
@@ -18,6 +22,8 @@ class NegociacaoController {
         let negociacao = this._criaNegociacao();
 
         this._listaNegociacoes.adiciona(negociacao);
+        this._mensagem.texto = 'Negociação adicionada com sucesso!';
+        this._mensagemView.update(this._mensagem);
         this._negocoesView.update(this._listaNegociacoes);
         this._limpaFormulario();
     }
